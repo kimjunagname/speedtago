8cd86cd1e790db4aa06948a2a6f9ce62a392117d
USER <USER@USERP>
2018-06-25 17:24:20 +0900

    test

--- src/com/speedtago/member/controller/MemberController.java
+++ src/com/speedtago/member/controller/MemberController.java
@@ -22,8 +22,7 @@ public class MemberController extends HttpServlet {
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
-		doGet(request, response);
+
 	}
 
 }
